092453428351f63e4d60f4e938bb0b3452b0c8f5
Giuseppe <giuseppe.digilio@4science.it>
2018-09-14 18:00:38 +0200

    reverted disabled routes and login by credentials

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,21 +6,21 @@ import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      { path: '', redirectTo: '/mydspace', pathMatch: 'full' },
-      // { path: 'home', loadChildren: './+home-page/home-page.module#HomePageModule' },
-      // { path: 'communities', loadChildren: './+community-page/community-page.module#CommunityPageModule' },
-      // { path: 'collections', loadChildren: './+collection-page/collection-page.module#CollectionPageModule' },
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: 'home', loadChildren: './+home-page/home-page.module#HomePageModule' },
+      { path: 'communities', loadChildren: './+community-page/community-page.module#CommunityPageModule' },
+      { path: 'collections', loadChildren: './+collection-page/collection-page.module#CollectionPageModule' },
       { path: 'items', loadChildren: './+item-page/item-page.module#ItemPageModule' },
       { path: 'mydspace', loadChildren: './+my-dspace-page/my-dspace-page.module#MyDSpacePageModule' },
-      // { path: 'search', loadChildren: './+search-page/search-page.module#SearchPageModule' },
-      // { path: 'admin', loadChildren: './+admin/admin.module#AdminModule' },
+      { path: 'search', loadChildren: './+search-page/search-page.module#SearchPageModule' },
+      { path: 'admin', loadChildren: './+admin/admin.module#AdminModule' },
       { path: 'login', loadChildren: './+login-page/login-page.module#LoginPageModule' },
       { path: 'logout', loadChildren: './+logout-page/logout-page.module#LogoutPageModule' },
       { path: 'submit', loadChildren: './+submit-page/submit-page.module#SubmitPageModule' },
       { path: 'workspaceitems', loadChildren: './+workspaceitems-edit-page/workspaceitems-edit-page.module#WorkspaceitemsEditPageModule' },
       { path: 'workflowitems', loadChildren: './+workflowitems-edit-page/workflowitems-edit-page.module#WorkflowitemsEditPageModule' },
       { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
-    ],{ enableTracing: true })
+    ])
   ],
   exports: [RouterModule]
 })
