5e339affd7d380e82ddcdaf6c271bc51ab2cf0e5
Giuseppe Digilio <giuseppe.digilio@4science.it>
2019-04-30 19:10:59 +0200

    Added view button for pool task

--- src/app/shared/object-list/my-dspace-result-list-element/pool-my-dspace-result/pool-my-dspace-result-list-element.component.ts
+++ src/app/shared/object-list/my-dspace-result-list-element/pool-my-dspace-result/pool-my-dspace-result-list-element.component.ts
@@ -1,18 +1,20 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 
-import { Observable } from 'rxjs';
-import { find } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { find, take } from 'rxjs/operators';
 
 import { renderElementsFor } from '../../../object-collection/shared/dso-element-decorator';
 import { MyDSpaceResultListElementComponent, } from '../my-dspace-result-list-element.component';
 import { ViewMode } from '../../../../core/shared/view-mode.model';
 import { RemoteData } from '../../../../core/data/remote-data';
-import { isNotUndefined } from '../../../empty.util';
+import { hasValue, isNotUndefined } from '../../../empty.util';
 import { ListableObject } from '../../../object-collection/shared/listable-object.model';
 import { Workflowitem } from '../../../../core/submission/models/workflowitem.model';
 import { PoolTask } from '../../../../core/tasks/models/pool-task-object.model';
 import { PoolTaskMyDSpaceResult } from '../../../object-collection/shared/pool-task-my-dspace-result.model';
 import { MyDspaceItemStatusType } from '../../../object-collection/shared/mydspace-item-status/my-dspace-item-status-type';
+import { MYDSPACE_ROUTE } from '../../../../+my-dspace-page/my-dspace-page.component';
 
 /**
  * This component renders pool task object for the mydspace result in the list view.
@@ -42,8 +44,13 @@ export class PoolMyDSpaceResultListElementComponent extends MyDSpaceResultListEl
    */
   public workflowitem: Workflowitem;
 
+  public viewMode: ViewMode = ViewMode.List;
+  private sub: Subscription;
+
   constructor(@Inject('objectElementProvider') public listable: ListableObject,
-              @Inject('indexElementProvider') public index: number) {
+              @Inject('indexElementProvider') public index: number,
+              private route: ActivatedRoute,
+              private router: Router) {
     super(listable, index);
   }
 
@@ -64,4 +71,32 @@ export class PoolMyDSpaceResultListElementComponent extends MyDSpaceResultListEl
       this.workflowitem = rd.payload;
     });
   }
+
+  view() {
+    this.sub = this.route.queryParams.pipe(
+      take(1))
+      .subscribe((params) => {
+        const pageSize = 1;
+        const queryPageSize = params.pageSize || 1;
+        const queryPage = params.page || 1;
+        const page = ((queryPage - 1) * queryPageSize) + (this.dsoIndex + 1);
+
+        const navigationExtras: NavigationExtras = {
+          queryParams: {
+            view: ViewMode.Detail,
+            page,
+            pageSize
+          },
+          queryParamsHandling: 'merge'
+        };
+        this.router.navigate([MYDSPACE_ROUTE], navigationExtras);
+      });
+  }
+
+  ngOnDestroy() {
+    if (hasValue(this.sub)) {
+      this.sub.unsubscribe();
+    }
+  }
+
 }
