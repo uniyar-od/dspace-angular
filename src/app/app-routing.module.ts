f88bdd72656e1f89fd131de8d8caefa412f94f8f
Giuseppe Digilio <giuseppe.digilio@4science.it>
2019-05-15 19:16:34 +0200

    changed default route to mydspace page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,15 +19,15 @@ export function getCommunityModulePath() {
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      { path: '', redirectTo: '/home', pathMatch: 'full' },
-      { path: 'home', loadChildren: './+home-page/home-page.module#HomePageModule' },
-      { path: COMMUNITY_MODULE_PATH, loadChildren: './+community-page/community-page.module#CommunityPageModule' },
-      { path: COLLECTION_MODULE_PATH, loadChildren: './+collection-page/collection-page.module#CollectionPageModule' },
+      { path: '', redirectTo: '/mydspace', pathMatch: 'full' },
+      // { path: 'home', loadChildren: './+home-page/home-page.module#HomePageModule' },
+      // { path: COMMUNITY_MODULE_PATH, loadChildren: './+community-page/community-page.module#CommunityPageModule' },
+      // { path: COLLECTION_MODULE_PATH, loadChildren: './+collection-page/collection-page.module#CollectionPageModule' },
       { path: ITEM_MODULE_PATH, loadChildren: './+item-page/item-page.module#ItemPageModule' },
       { path: 'mydspace', loadChildren: './+my-dspace-page/my-dspace-page.module#MyDSpacePageModule', canActivate: [AuthenticatedGuard] },
-      { path: 'search', loadChildren: './+search-page/search-page.module#SearchPageModule' },
-      { path: 'browse', loadChildren: './+browse-by/browse-by.module#BrowseByModule' },
-      { path: 'admin', loadChildren: './+admin/admin.module#AdminModule', canActivate: [AuthenticatedGuard] },
+      // { path: 'search', loadChildren: './+search-page/search-page.module#SearchPageModule' },
+      // { path: 'browse', loadChildren: './+browse-by/browse-by.module#BrowseByModule' },
+      // { path: 'admin', loadChildren: './+admin/admin.module#AdminModule', canActivate: [AuthenticatedGuard] },
       { path: 'login', loadChildren: './+login-page/login-page.module#LoginPageModule' },
       { path: 'logout', loadChildren: './+logout-page/logout-page.module#LogoutPageModule' },
       { path: 'submit', loadChildren: './+submit-page/submit-page.module#SubmitPageModule' },
