3d352f03d6b30662c459ca97d1f30a81614afc93
Giuseppe Digilio <giuseppe.digilio@4science.it>
2020-01-24 14:54:10 +0100

    fixed WorkspaceItemRejectedSearchResultListElementComponent

--- src/app/shared/object-list/my-dspace-result-list-element/workspace-item-rejected-search-result/workspace-item-rejected-search-result-list-element.component.ts
+++ src/app/shared/object-list/my-dspace-result-list-element/workspace-item-rejected-search-result/workspace-item-rejected-search-result-list-element.component.ts
@@ -14,6 +14,9 @@ import { ViewMode } from '../../../../core/shared/view-mode.model';
 import { Context } from '../../../../core/shared/context.model';
 import { WorkspaceItemSearchResult } from '../../../object-collection/shared/workspace-item-search-result.model';
 
+/**
+ * This component renders workspaceitem object for the search result in the list view.
+ */
 @Component({
   selector: 'ds-workspaceitem-my-dspace-result-list-element',
   styleUrls: ['../../search-result-list-element/search-result-list-element.component.scss', './workspace-item-rejected-search-result-list-element.component.scss'],
@@ -23,10 +26,21 @@ import { WorkspaceItemSearchResult } from '../../../object-collection/shared/wor
 @listableObjectComponent(WorkspaceItemSearchResult, ViewMode.ListElement, Context.Workflow)
 export class WorkspaceItemRejectedSearchResultListElementComponent extends SearchResultListElementComponent<WorkspaceItemSearchResult, WorkspaceItem> {
 
+  /**
+   * The item object that belonging to the result object
+   */
   item: Item;
+
+  /**
+   * Represent item's status
+   */
   status = MyDspaceItemStatusType.REJECTED;
 
+  /**
+   * Initialize all instance variables
+   */
   ngOnInit() {
+    super.ngOnInit();
     this.initItem(this.dso.item as Observable<RemoteData<Item>>);
   }
 
