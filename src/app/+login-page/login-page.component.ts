70db2b804de06065cc428d9d1d4c3bb18f837682
Giuseppe Digilio <giuseppe.digilio@4science.it>
2020-04-28 15:00:28 +0200

    [CST-2892] Fixed issue with authentication without SSR

--- src/app/+login-page/login-page.component.ts
+++ src/app/+login-page/login-page.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 
 import { combineLatest as observableCombineLatest, Subscription } from 'rxjs';
 import { filter, find, take } from 'rxjs/operators';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 
 import { AppState } from '../app.reducer';
 import {
@@ -14,7 +14,7 @@ import {
 } from '../core/auth/auth.actions';
 import { hasValue, isEmpty, isNotEmpty } from '../shared/empty.util';
 import { AuthTokenInfo } from '../core/auth/models/auth-token-info.model';
-import { getSSOLoginUrl, isAuthenticated } from '../core/auth/selectors';
+import { getSSOLoginUrl, isAuthenticated, isAuthenticationLoading } from '../core/auth/selectors';
 import { ServerResponseService } from '../core/services/server-response.service';
 import { AuthService } from '../core/auth/auth.service';
 
@@ -45,47 +45,49 @@ export class LoginPageComponent implements OnDestroy, OnInit {
   constructor(private route: ActivatedRoute,
               private authService: AuthService,
               private responseService: ServerResponseService,
-              private store: Store<AppState>) {}
+              private store: Store<AppState>) {
+  }
 
   /**
    * Initialize instance variables
    */
   ngOnInit() {
     const queryParamsObs = this.route.queryParams;
-    const authenticated = this.store.select(isAuthenticated);
-    const SSOLoginUrl = this.store.select(getSSOLoginUrl);
-    this.subs.push(observableCombineLatest(queryParamsObs, authenticated).pipe(
-      filter(([params, auth]) => isNotEmpty(params.token) || isNotEmpty(params.expired)),
+    const authenticated = this.store.pipe(select(isAuthenticated));
+    const authenticationLoading = this.store.pipe(select(isAuthenticationLoading));
+    const SSOLoginUrl = this.store.pipe(select(getSSOLoginUrl));
+    this.subs.push(observableCombineLatest(authenticationLoading, queryParamsObs, authenticated).pipe(
+      filter(([authLoading, params, auth]) => !authLoading && (isNotEmpty(params.token) || isNotEmpty(params.expired))),
       take(1)
-    ).subscribe(([params, auth]) => {
-      const token = params.token;
-      const redirectUrl = params.redirectUrl;
-      let authToken: AuthTokenInfo;
+      ).subscribe(([authLoading, params, auth]) => {
+        const token = params.token;
+        const redirectUrl = params.redirectUrl;
+        let authToken: AuthTokenInfo;
 
-      if (isNotEmpty(redirectUrl)) {
-        this.authService.setRedirectUrl(redirectUrl);
-      }
-      if (!auth) {
-        if (isNotEmpty(token)) {
-          authToken = new AuthTokenInfo(token);
-          this.store.dispatch(new AuthenticatedAction(authToken));
-        } else if (isNotEmpty(params.expired)) {
-          this.store.dispatch(new AddAuthenticationMessageAction('auth.messages.expired'));
+        if (isNotEmpty(redirectUrl)) {
+          this.authService.setRedirectUrl(redirectUrl);
         }
-      } else {
-        if (isNotEmpty(token)) {
-          authToken = new AuthTokenInfo(token);
-          this.store.dispatch(new AuthenticationSuccessAction(authToken));
+        if (!auth) {
+          if (isNotEmpty(token)) {
+            authToken = new AuthTokenInfo(token);
+            this.store.dispatch(new AuthenticatedAction(authToken));
+          } else if (isNotEmpty(params.expired)) {
+            this.store.dispatch(new AddAuthenticationMessageAction('auth.messages.expired'));
+          }
+        } else {
+          if (isNotEmpty(token)) {
+            authToken = new AuthTokenInfo(token);
+            this.store.dispatch(new AuthenticationSuccessAction(authToken));
+          }
         }
-      }
-    }),
+      }),
       observableCombineLatest(queryParamsObs, authenticated, SSOLoginUrl).pipe(
         find(([params, auth, url]) => isEmpty(params) && isNotEmpty(url))
       ).subscribe(([params, auth, url]) => {
-          if (!auth) {
-            this.responseService.redirect(url);
-          }
-        })
+        if (!auth) {
+          this.responseService.redirect(url);
+        }
+      })
     )
   }
 
